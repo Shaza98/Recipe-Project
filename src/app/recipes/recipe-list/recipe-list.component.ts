54b367f1354ad06846170212f43bb5b384c4980f
Shaza98 <mansourshaza@yahoo.com>
2018-10-09 21:50:56 +0200

    project

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [new Recipe('A test Recipe', 'This is a test','https://www.kitchensanctuary.com/wp-content/uploads/2017/05/Mexican-Chicken-Kebabs-with-Picante-Salsa-Recipe-tall2.jpg'),
+  new Recipe('A test Recipe', 'This is a test','https://www.kitchensanctuary.com/wp-content/uploads/2017/05/Mexican-Chicken-Kebabs-with-Picante-Salsa-Recipe-tall2.jpg')];
   constructor() { }
 
   ngOnInit() {
