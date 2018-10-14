077ba7813680bf852daf6a139c5fcdf9306305b8
Shaza98 <mansourshaza@yahoo.com>
2018-10-14 23:42:05 +0200

    recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [new Recipe('A test Recipe', 'This is a test','https://www.kitchensanctuary.com/wp-content/uploads/2017/05/Mexican-Chicken-Kebabs-with-Picante-Salsa-Recipe-tall2.jpg'),
   new Recipe('A test Recipe', 'This is a test','https://www.kitchensanctuary.com/wp-content/uploads/2017/05/Mexican-Chicken-Kebabs-with-Picante-Salsa-Recipe-tall2.jpg')];
   constructor() { }
@@ -14,4 +15,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
